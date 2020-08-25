8a63b1414ceb860b5ac79b1edaa3a7c531811bbf
blulady <blulady@gmail.com>
2020-08-25 09:58:57 -0700

    Create os167.py

---
+++ os167.py
@@ -0,0 +1,25 @@
+
+
+
+import os, sys, time
+
+path = "C:\python_projects"
+path2 = "C:\python_projects\doc.txt"
+file1 = 'new.txt'
+
+whoo = os.path.join(path, file1)
+
+dirs = os.listdir( path )
+
+for file in dirs:
+    print(file)
+
+moar_time = os.path.getmtime(whoo)
+fun_time = time.ctime(moar_time)
+
+modification_time = os.path.getmtime(path2)
+local_time = time.ctime(modification_time)
+print(path2 + " modified " + local_time)
+
+print(whoo + " modified " + fun_time)
+
