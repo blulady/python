61a56a09e46d09e480aeac600e8add6ddfae4867
blulady <blulady@gmail.com>
2020-08-26 16:41:28 -0700

    attempt

--- python171.py
+++ python171.py
@@ -5,27 +5,29 @@ import os
 path = "C:\\python_projects\\filelist"
 dirs = os.listdir( path )
 
-for file in dirs:
-    if file.endswith(".txt"):
-       print(file)
 
-conn = sqlite3.connect('test3.db')
+conn = sqlite3.connect('test7.db')
 cur = conn.cursor()
 
 def create_table():
+    conn = sqlite3.connect('test7.db')
+    cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS tbl_txtfile( ID INTEGER PRIMARY KEY AUTOINCREMENT, col_filename)')
-    
-def tablestuff():
- 
-    cur.execute("INSERT INTO tbl_textfile(col_filename) VALUE (?)",('info'))
     conn.commit()
-    c.close()
-    conn.close()
+
+def ad():
+    for file in dirs:
+        if file.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_textfile(col_fulename) VALUES(?)", ('file'))
+            print(file)   
+    conn.commit()
+conn.close()
+
 
 
 
 create_table()
-tablestuff()
+
 
 
     
